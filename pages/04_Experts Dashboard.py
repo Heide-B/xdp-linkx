64500f91ec0812d7529214539efd1af9baca4a63
Heide Balcera <77626580+Heide-B@users.noreply.github.com>
2022-06-18 14:38:46 +0800

    Added footer redirect

--- pages/04_Experts Dashboard.py
+++ pages/04_Experts Dashboard.py
@@ -92,7 +92,7 @@ text-align: center;
 }
 </style>
 <div class="footer">
-<p><a href="/">Return Home</a>
+<p><a href="https://share.streamlit.io/heide-b/xdp-linkx/main/xdp.py">Return Home</a>
 <p>Developed with ❤ by</p>
 <p><strong>LinkX: Log and Information Exchange</strong></p>
 </div>
